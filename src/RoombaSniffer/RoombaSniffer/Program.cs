c149c4d2cae07881aa224d9de49dd90182a60f06
RobotFreak <p.recktenwald@gmail.com>
2010-05-15 15:28:26 +0200

    sniffer initial release

---
+++ src/RoombaSniffer/RoombaSniffer/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.Ethernet;
+
+namespace RoombaSniffer
+{
+    class Program
+    {
+        static bool _continue;
+        static SerialPort _serialPort;
+//        static Crc16 _crc16;
+
+        static void Main(string[] args)
+        {
+            Boolean done;
+            TextWriterTraceListener tl =
+                new TextWriterTraceListener(@"C:\roombalog.txt");
+            Trace.Listeners.Add(tl);
+
+
+
+            ConsoleKeyInfo cki;
+            //string name;
+            //string message;
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+            Thread readThread = new Thread(Read);
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();
+
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = SetPortName(_serialPort.PortName);
+            //_serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate);
+            //_serialPort.Parity = SetPortParity(_serialPort.Parity);
+            //_serialPort.DataBits = SetPortDataBits(_serialPort.DataBits);
+            //_serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
+            //_serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
+
+            _serialPort.WriteLine("h");
+            _serialPort.WriteLine("m");
+            _serialPort.WriteLine("s");
+            _serialPort.WriteLine("c15");
+            _serialPort.WriteLine("m");
+//            _serialPort.WriteLine("");
+
+            // Retrieve the device list from the local machine
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+                Console.Write((i + 1) + ". " + device.Name);
+                if (device.Description != null)
+                    Console.WriteLine(" (" + device.Description + ")");
+                else
+                    Console.WriteLine(" (No description available)");
+            }
+
+            int deviceIndex = 0;
+            do
+            {
+                Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
+                string deviceIndexString = Console.ReadLine();
+                if (!int.TryParse(deviceIndexString, out deviceIndex) ||
+                    deviceIndex < 1 || deviceIndex > allDevices.Count)
+                {
+                    deviceIndex = 0;
+                }
+            } while (deviceIndex == 0);
+
+            // Take the selected adapter
+            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+
+            // Open the device
+            using (PacketCommunicator communicator = 
+                selectedDevice.Open(65536,                                  // portion of the packet to capture
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    1000))                                  // read timeout
+            {
+                Console.WriteLine("Listening on " + selectedDevice.Description + "...");
+                Trace.WriteLine("Listening on " + selectedDevice.Description + "...");
+
+                // start the capture
+                done = false;
+                do
+                {
+                    communicator.ReceivePackets(1, PacketHandler);
+                    if (Console.KeyAvailable)
+                    {
+                        cki = Console.ReadKey(true);
+                        if (cki.Key == ConsoleKey.Escape)
+                            done = true;
+                    }
+                }
+                while (done == false);
+            }
+            tl.Flush();
+            tl.Close();
+            readThread.Join();
+            _serialPort.Close();
+
+        }
+
+        // Callback function invoked by Pcap.Net for every incoming packet
+        private static void PacketHandler(Packet packet)
+        {
+            EthernetDatagram eth = packet.Ethernet;
+            Crc16 _crc16 = new Crc16(InitialCrcValue.Zeros);
+            UInt16 crc;
+
+            Console.Write(packet.Timestamp.ToString("hh:mm:ss.fff"));
+            Trace.Write(packet.Timestamp.ToString("hh:mm:ss.fff"));
+            const int LineLength = 16;
+            for (int i = 0; i < eth.PayloadLength; i++)
+            {
+                Console.Write(" " + eth.Payload[i].ToString("X2"));
+                Trace.Write(" " + eth.Payload[i].ToString("X2"));
+                if ((i + 1) % LineLength == 0)
+                {
+                    Console.WriteLine();
+                    Trace.WriteLine("");
+                    Console.Write("            ");
+                    Trace.Write("            ");
+                }
+//                if (i < eth.PayloadLength - 2)
+//                    data[i] = eth.Payload[i];
+            }
+            Console.Write(" (" + eth.PayloadLength + " Bytes)");
+            Trace.Write(" (" + eth.PayloadLength + " Bytes)");
+            byte[] data = new byte[eth.PayloadLength-2];
+            for (int i = 0; i < eth.PayloadLength - 2; i++)
+            {
+                data[i] = eth.Payload[i];
+            }
+            crc = _crc16.ComputeChecksum(data);
+            Console.WriteLine(" (CRC: " + crc.ToString("X4") + ")");
+            Trace.WriteLine(" (CRC: " + crc.ToString("X4") + ")");
+        }
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        public static string SetPortName(string defaultPortName)
+        {
+            string portName;
+
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("COM port({0}): ", defaultPortName);
+            portName = Console.ReadLine();
+
+            if (portName == "")
+            {
+                portName = defaultPortName;
+            }
+            return portName;
+        }
+
+        public static int SetPortBaudRate(int defaultPortBaudRate)
+        {
+            string baudRate;
+
+            Console.Write("Baud Rate({0}): ", defaultPortBaudRate);
+            baudRate = Console.ReadLine();
+
+            if (baudRate == "")
+            {
+                baudRate = defaultPortBaudRate.ToString();
+            }
+
+            return int.Parse(baudRate);
+        }
+
+        public static Parity SetPortParity(Parity defaultPortParity)
+        {
+            string parity;
+
+            Console.WriteLine("Available Parity options:");
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Parity({0}):", defaultPortParity.ToString());
+            parity = Console.ReadLine();
+
+            if (parity == "")
+            {
+                parity = defaultPortParity.ToString();
+            }
+
+            return (Parity)Enum.Parse(typeof(Parity), parity);
+        }
+
+        public static int SetPortDataBits(int defaultPortDataBits)
+        {
+            string dataBits;
+
+            Console.Write("Data Bits({0}): ", defaultPortDataBits);
+            dataBits = Console.ReadLine();
+
+            if (dataBits == "")
+            {
+                dataBits = defaultPortDataBits.ToString();
+            }
+
+            return int.Parse(dataBits);
+        }
+
+        public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
+        {
+            string stopBits;
+
+            Console.WriteLine("Available Stop Bits options:");
+            foreach (string s in Enum.GetNames(typeof(StopBits)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Stop Bits({0}):", defaultPortStopBits.ToString());
+            stopBits = Console.ReadLine();
+
+            if (stopBits == "")
+            {
+                stopBits = defaultPortStopBits.ToString();
+            }
+
+            return (StopBits)Enum.Parse(typeof(StopBits), stopBits);
+        }
+
+        public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
+        {
+            string handshake;
+
+            Console.WriteLine("Available Handshake options:");
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Stop Bits({0}):", defaultPortHandshake.ToString());
+            handshake = Console.ReadLine();
+
+            if (handshake == "")
+            {
+                handshake = defaultPortHandshake.ToString();
+            }
+
+            return (Handshake)Enum.Parse(typeof(Handshake), handshake);
+        }
+    }
+}
